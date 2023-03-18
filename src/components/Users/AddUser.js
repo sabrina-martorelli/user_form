4040c78ed173c6882b08e473d225c337d0e861ea
SLM <sabrina.martorelli@gmail.com>
2023-03-18 19:49:13 +0000

    Adds Card component and css and connects css styles. Adds button js and css files

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
-const AddUser = () => {
-    return (  
+import Card from '../UI/Card'
 
-        <form>
+import styles from './AddUser.module.css';
+
+
+
+const AddUser = () => {
+
+const addUserHandler =(event) => {
+
+    event.preventDefault();
+
+}
+
+
+    return (  
+    <Card myClasses={styles.input}>
+       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
         <input id="username" type="text" />
 
         <label htmlFor='age'>Age(Years)</label>
         <input id="age" type="number" />
 
-        <button type="button"></button>
-
+        <button type="button">Add user</button>
 
         </form>
-
-
+    </Card>
     );
 }
  
